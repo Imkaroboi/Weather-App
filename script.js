887ddefac1d9c63c2e26e107b536176b944a5289
Imkaroboi <sarveshshirashyad@gmail.com>
2024-11-03 22:39:58 -0800

    added javascript objects

---
+++ script.js
@@ -0,0 +1,6 @@
+const apiKey = '7cf0cc5cbd2ee69957eb610f405c871b';
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=fremont";
+
+async function checkWeather() {
+    const response = await fetch()
+};
